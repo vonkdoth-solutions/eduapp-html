2cec0273eab2fbfb32f4a372810a713d2a9a0a7b
Sravan <webdeveloper.sravan@gmail.com>
2020-05-27 16:47:12 +0530

    updated html 27/05

---
+++ nikath/js/hscript.js
@@ -0,0 +1,10 @@
+$(".panel-left").resizable({
+   handleSelector: ".splitter",
+   resizeHeight: false
+ });
+
+ $(".panel-top").resizable({
+   handleSelector: ".splitter-horizontal",
+   resizeWidth: false
+ });
+ 
